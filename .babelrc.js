088d413f108d44c500cf17760fd978496ff0170f
丁鑫 <cindy13@sfmail.sf-express.com>
2022-01-06 15:43:34 +0800

    feat:只进行helper函数抽离

--- .babelrc.js
+++ .babelrc.js
@@ -1,13 +1,4 @@
 module.exports = {
   presets: ['@babel/env', '@babel/typescript', '@babel/react'],
-  plugins: [
-    '@babel/proposal-class-properties',
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        corejs: 3,
-        helpers: true,
-      },
-    ],
-  ],
+  plugins: ['@babel/plugin-transform-runtime', '@babel/proposal-class-properties'],
 };
