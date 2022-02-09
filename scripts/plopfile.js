d0fc3722d5add648009543f71d47a34d87d393eb
cindydove <dindydove@163.com>
2022-02-09 23:23:01 +0800

    feat:修改批量新增模板文件

--- scripts/plopfile.js
+++ scripts/plopfile.js
@@ -1,8 +1,7 @@
 /* eslint-disable  import/no-extraneous-dependencies */
-import path from 'path';
-const __dirname = path.resolve(path.dirname(''))
+const path = require("path")
 console.log("dx---__dirname",__dirname)
-export default function (plop) {
+const plop =  function (plop) {
   plop.setGenerator('component', {
     description: '创建一个新组件',
     prompts: [
@@ -11,50 +10,52 @@ export default function (plop) {
     actions: [
       {
         type: 'add',
-        path: path.resolve(__dirname, './src/{{pascalCase name}}/index.ts'),
-        templateFile: path.resolve(__dirname, './templates/component/index.hbs'),
+        path: path.resolve(__dirname, '../src/{{pascalCase name}}/index.ts'),
+        templateFile: path.resolve(__dirname, '../templates/component/index.hbs'),
       },
       {
         type: 'add',
-        path: path.resolve(__dirname, './src/{{pascalCase name}}/{{pascalCase name}}.tsx'),
-        templateFile: path.resolve(__dirname, './templates/component/comp.hbs'),
+        path: path.resolve(__dirname, '../src/{{pascalCase name}}/{{pascalCase name}}.tsx'),
+        templateFile: path.resolve(__dirname, '../templates/component/comp.hbs'),
       },
       {
         type: 'add',
-        path: path.resolve(__dirname, './src/{{pascalCase name}}/style/index.less'),
-        templateFile: path.resolve(__dirname, './templates/component/style/style.hbs'),
+        path: path.resolve(__dirname, '../src/{{pascalCase name}}/style/index.less'),
+        templateFile: path.resolve(__dirname, '../templates/component/style/style.hbs'),
       },
       {
         type: 'add',
-        path: path.resolve(__dirname, './src/{{pascalCase name}}/style/index.ts'),
-        templateFile: path.resolve(__dirname, './templates/component/style/index.hbs'),
+        path: path.resolve(__dirname, '../src/{{pascalCase name}}/style/index.ts'),
+        templateFile: path.resolve(__dirname, '../templates/component/style/index.hbs'),
       },
       {
         type: 'add',
-        path: path.resolve(__dirname, './docs/components/{{pascalCase name}}.md'),
-        templateFile: path.resolve(__dirname, './templates/component/doc.hbs'),
+        path: path.resolve(__dirname, '../docs/components/{{pascalCase name}}.md'),
+        templateFile: path.resolve(__dirname, '../templates/component/doc.hbs'),
       },
       {
         type: 'add',
-        path: path.resolve(__dirname, './src/{{pascalCase name}}/interface.ts'),
-        templateFile: path.resolve(__dirname, './templates/component/interface.hbs'),
+        path: path.resolve(__dirname, '../src/{{pascalCase name}}/interface.ts'),
+        templateFile: path.resolve(__dirname, '../templates/component/interface.hbs'),
       },
       {
         type: 'add',
-        path: path.resolve(__dirname, './src/{{pascalCase name}}/demo/basic.tsx'),
-        templateFile: path.resolve(__dirname, './templates/component/demo/basic.hbs'),
+        path: path.resolve(__dirname, '../src/{{pascalCase name}}/demo/basic.tsx'),
+        templateFile: path.resolve(__dirname, '../templates/component/demo/basic.hbs'),
       },
       {
         type: 'add',
-        path: path.resolve(__dirname, './src/{{pascalCase name}}/__tests__/index.test.tsx'),
-        templateFile: path.resolve(__dirname, './templates/component/__tests__/index.test.hbs'),
+        path: path.resolve(__dirname, '../src/{{pascalCase name}}/__tests__/index.test.tsx'),
+        templateFile: path.resolve(__dirname, '../templates/component/__tests__/index.test.hbs'),
       },
       {
         type: 'append',
-        path: path.resolve(__dirname, './src/index.ts'),
+        path: path.resolve(__dirname, '../src/index.ts'),
         pattern: '/* PLOP_INJECT_EXPORT */',
         template: "export { default as {{pascalCase name}} } from './{{pascalCase name}}';",
       },
     ],
   });
 }
+
+module.exports = plop
